8d916ecad09ca94072add5e7a0e259459869925c
Jardan Alexandr <147718215+H4rmon1st@users.noreply.github.com>
2025-04-26 13:40:52 +0300

    Add files via upload

---
+++ NoteFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class NoteFactory
+{
+    public static Note CreateNote(string type, string title, string content)
+    {
+        if (type == "Simple")
+            return new SimpleNote { Title = title, Content = content };
+        else if (type == "Important")
+            return new ImportantNote { Title = title, Content = content };
+        else
+            throw new ArgumentException("Tip de notă invalid.");
+    }
+}
---
+++ SimpleNote.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class SimpleNote : Note
+{
+    public override void Display()
+    {
+        Console.WriteLine($"Simple Note: {Title} - {Content}");
+    }
+}
